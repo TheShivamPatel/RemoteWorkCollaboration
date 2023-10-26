345eab95c69962f18d6a5f4b5a383eef001ac4fd
SHIVAM <shivampatel0700@gmail.com>
2023-10-26 18:26:19 +0530

    Image picked from gallery.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatterbox/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -71,6 +72,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if (!_isLogin) const UserImagePicker(),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Address',
